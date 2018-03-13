a345bb3e12ae732eb2d9211de3c01fe9099ca4f6
Emmanuel Nwogbo <emmnlchuck@gmail.com>
2018-03-14 00:20:03 +0100

    set up routes folder and files

---
+++ server/lib/index.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+
+import business from '../routes/business';
+import index from '../routes/index';
+import user from '../routes/user';
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(cors());
+
+
+app.use('/api/v1/businesses/', business);
+app.use('/', index);
+app.use('/api/v1/auth/', user);
+
+const port = 3000;
+
+app.listen(port, function(error) {
+  if (error) {
+    console.log('error');
+  } else {
+    console.log(`server listening on port ${port}`);
+  }
+});
+
+export default app;
