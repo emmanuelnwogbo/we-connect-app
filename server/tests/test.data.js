746709d89837e9c5866967ff9d27125f45c9107a
Emmanuel Nwogbo <emmnlchuck@gmail.com>
2018-03-16 09:52:04 +0100

    refactored tests and moved test data to separate file

---
+++ server/tests/test.data.js
@@ -0,0 +1,59 @@
+let signupDetails = {
+  firstname: 'paul',
+  lastname: 'doguns',
+  email: 'dogun@gmail.com',
+  password: 'eemmaa',
+  confirmpassword: 'eemmaa',
+};
+
+let signupDetailsTwo = {
+  firstname: 'paul',
+  lastname: '',
+  email: 'dogun@gmail.com',
+  password: 'eemmaa',
+  confirmpassword: 'eemmaa',
+};
+
+let signupDetailsThree = {
+  firstname: 'paul',
+  lastname: 'jerry',
+  email: 'dogungmail.com',
+  password: 'eemmaa',
+  confirmpassword: 'eemmaa',
+};
+
+let signupDetailsFour = {
+  firstname: 'paul',
+  lastname: 'jerry',
+  email: 'dogun@gmail.com',
+  password: '',
+  confirmpassword: 'eemmaa',
+};
+
+let signinDetails = {
+  email: 'dogun@gmail.com',
+  password: 'eemmaa',
+  id: 1,
+};
+
+let signinDetailsTwo = {
+  email: 'dogun@gmail.com',
+  password: 'eema',
+  id: 1,
+};
+
+let signinDetailsThree = {
+  email: 'dogun@gmail.com',
+  password: 'eemmmaa',
+  id: 1,
+};
+
+export default {
+  signupDetails,
+  signupDetailsTwo,
+  signupDetailsThree,
+  signupDetailsFour,
+  signinDetails,
+  signinDetailsTwo,
+  signinDetailsThree
+}
--- server/tests/user.controllers.test.js
+++ server/tests/user.controllers.test.js
@@ -8,57 +8,16 @@ let chaiShould = should();
 
 chai.use(chaiHttp);
 
-let signupDetails = {
-  firstname: 'paul',
-  lastname: 'doguns',
-  email: 'dogun@gmail.com',
-  password: 'eemmaa',
-  confirmpassword: 'eemmaa',
-};
-
-let signupDetailsTwo = {
-  firstname: 'paul',
-  lastname: '',
-  email: 'dogun@gmail.com',
-  password: 'eemmaa',
-  confirmpassword: 'eemmaa',
-};
-
-let signupDetailsThree = {
-  firstname: 'paul',
-  lastname: 'jerry',
-  email: 'dogungmail.com',
-  password: 'eemmaa',
-  confirmpassword: 'eemmaa',
-};
-
-let signupDetailsFour = {
-  firstname: 'paul',
-  lastname: 'jerry',
-  email: 'dogun@gmail.com',
-  password: '',
-  confirmpassword: 'eemmaa',
-};
-
-let signinDetails = {
-  email: 'dogun@gmail.com',
-  password: 'eemmaa',
-  id: 1,
-};
-
-let signinDetailsTwo = {
-  email: 'dogun@gmail.com',
-  password: 'eema',
-  id: 1,
-};
-
-let signinDetailsThree = {
-  email: 'dogun@gmail.com',
-  password: 'eemmmaa',
-  id: 1,
-};
-
-import dummyData from '../dummydb/dummydata';
+import testdata from './test.data';
+const {
+  signupDetails,
+  signupDetailsTwo,
+  signupDetailsThree,
+  signupDetailsFour,
+  signinDetails,
+  signinDetailsTwo,
+  signinDetailsThree
+} = testdata;
 
 describe('/api/v1/auth/signup', () => {
   it('should let a new user signup after all fields are completed adequately', done => {
